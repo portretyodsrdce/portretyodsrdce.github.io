d81740d113c4e22f7fb493ae826fde565959d19d
portretyodsrdce <julca.vackova@atlas.cz>
2025-06-07 18:14:35 +0200

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  7 17:48:14 2025
+
+@author: matej
+"""
+if (window.location.hostname === 'portretyodsrdce.github.io') {
+  window.location.href = 'https://portretyodsrdce.cz' + window.location.pathname + window.location.search + window.location.hash;
+}
+

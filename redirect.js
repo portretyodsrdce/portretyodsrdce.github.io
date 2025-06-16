32a94eb5f8bbe6e75d3d2b7a7d2c18ded391dc0c
portretyodsrdce <julca.vackova@atlas.cz>
2025-06-16 18:22:17 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,6 +1,12 @@
 // redirect.js
 (function() {
-  const langFragment = window.location.hash; // zachytí #en, #cs, nebo prázdné
-  const target = 'https://portretyodsrdce.cz/' + langFragment;
-  window.location.replace(target);
+  if (window.location.hostname === "portretyodsrdce.github.io") {
+    // redirect pouze pokud je hash prázdný (tedy uživatel právě přišel na základní stránku)
+    if (!window.location.hash) {
+      const langFragment = window.location.hash; // tady prázdné, ale pro jistotu
+      const target = 'https://portretyodsrdce.cz/' + langFragment;
+      window.location.replace(target);
+    }
+    // Pokud je fragment (#en, #cs), redirect neprovádět, už je na správné stránce
+  }
 })();
